dab06773c3678c80b7240bc47a28a2a6b0000b08
Anatolii Yakushko <shaddyx@gmail.com>
2017-10-07 15:14:35 +0300

    [+] added try/catch to loader

--- js/gameLoader.js
+++ js/gameLoader.js
@@ -1,10 +1,10 @@
 "use strict"
-//
-// var _boot$gameLogic = function() {
-//     console.log("aaaaaaaaaaaaaaaaa");
-//     // load all dna and libraries
-//     //_.log.debug('gameloader.js',  'loading game...')
-//
-//     //_.load(_.env.basepath + 'sys.js')
-//     //_.load(_.env.basepath + 'lib/math.js')
-// };
+
+var _boot$gameLogic = function() {
+    console.log("aaaaaaaaaaaaaaaaa");
+    // load all dna and libraries
+    //_.log.debug('gameloader.js',  'loading game...')
+
+    //_.load(_.env.basepath + 'sys.js')
+    //_.load(_.env.basepath + 'lib/math.js')
+};
