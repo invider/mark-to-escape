3697f443d104e479061d16b8aa5875f03af1f96a
Anatolii Yakushko <shaddyx@gmail.com>
2017-10-07 19:09:08 +0300

    [-] fixed objectToSpawn

--- js/spawner.js
+++ js/spawner.js
@@ -5,11 +5,14 @@
 this['@lib/spawnLevel'] = function(lvl){
     var my = this;
     return this._.lib.parser.parse(this._.selectOne("lib/levels/level" + lvl), function(x, y, type, params){
+        var objectToSpawn = false;
         if (type == '*'){
             console.log("Spawning:", x, y, type, params);
-            let e = my._.sys.spawn('dna/levelWall', 'lab/camera');
-            e.x = x;
-            e.y = y;
+            objectToSpawn = my._.sys.spawn('dna/levelWall', 'lab/camera');
+        }
+        if (objectToSpawn){
+            objectToSpawn.x = x;
+            objectToSpawn.y = y;
         }
     });
 };
