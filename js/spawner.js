fab7a8fdfc2e87a3a2e9dfcf8bb962236fc5d800
Anatolii Yakushko <shaddyx@gmail.com>
2017-10-07 21:56:46 +0300

    [-] fixed marks

--- js/spawner.js
+++ js/spawner.js
@@ -12,10 +12,17 @@ this['@lib/spawnLevel'] = function(lvl){
         '<': 'leftSpawn',
         '^': 'upSpawn',
         'v': 'downSpawn',
+        'R': 'rightMark',
+        'L': 'leftMark',
+        'U': 'upMark',
+        'D': 'downMark',
         'X': 'exit'
     };
     return this._.lib.parser.parse(this._.selectOne("lib/levels/level" + lvl), function(x, y, type, params, param) {
         let dna = typeMap[type];
+        if (dna == "downMark"){
+            debugger;
+        }
         if (dna){
             let entity = my._.sys.spawn('dna/' + dna, 'lab/camera', {
                 x: x,
@@ -24,6 +31,10 @@ this['@lib/spawnLevel'] = function(lvl){
             if (dna === 'player') {
                 my._.lab.camera.follow(entity)
             }
+        } else if (type.length == 0){
+            console.error("Type is empty")
+        }  else if (type != " "){
+                console.error("No such type [" + type + "]")
         }
     });
 };
