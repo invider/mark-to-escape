818d09bb7333d9ff4f766b58c4f232ef89f0fcb4
Anatolii Yakushko <shaddyx@gmail.com>
2017-10-07 21:21:05 +0300

    [-] removed unused macro from lvl2

--- js/levels/level2.js
+++ js/levels/level2.js
@@ -4,12 +4,11 @@
     "env/player/markers/up=1\n"+
     "env/player/goal=10\n"+
     "^^^SETTINGS^^^"+
-    "M_D_@100" +
     "^^^MACROS^^^" +
     "*****************************************************\n" +
     "*    >            w                                 *\n" +
     "*                 w     wwwwwwwwwwwwwwwww           *\n" +
-    "*             D   w     w               w           *\n" +
+    "*             @   w     w               w           *\n" +
     "*wwwww wwwwwwwwwwww     w   wwwwwwwww   w           *\n" +
     "*    w w                w   w       w   w           *\n" +
     "*    w wwwwwwwwwwwwwwwwww   w       w   wwwwwwwwwww *\n" +
