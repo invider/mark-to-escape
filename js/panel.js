3fd5618b914e2a7295305f6360b4d71750fd5d47
bsheludchenko <bsheludchenko@cogniance.com>
2017-10-08 22:23:22 +0300

    panel

--- js/panel.js
+++ js/panel.js
@@ -1,22 +1,28 @@
 this['@dna/panel'] = function(_) {
 
 	var menu = _.lib.menu([ {
+		img: _.res.up_mark,
 		value : constants.objects.upMark,
 		path : constants.path.UP_MARKERS_COUNT
 	}, {
+		img: _.res.down_mark,
 		value : constants.objects.downMark,
 		path : constants.path.DOWN_MARKERS_COUNT
 	}, {
+		img: _.res.left_mark,
 		value : constants.objects.leftMark,
 		path : constants.path.LEFT_MARKERS_COUNT
 	}, {
+		img: _.res.right_mark,
 		value : constants.objects.rightMark,
 		path : constants.path.RIGHT_MARKERS_COUNT
 	}, {
+		img: _.res.speedupMark,
 		value : constants.objects.speedUpMark,
 		path : constants.path.SPEED_UP_MARKERS_COUNT
 	} ].map(function(i) {
 		return {
+			img: i.img,
 			value : i.value,
 			canSelect : function() {
 				return _.lib.selectUtils.value(i.path)
@@ -41,14 +47,14 @@ this['@dna/panel'] = function(_) {
 
 		draw : function(ctx) {
 			var items = menu.items()
-			var w = 70, s = 10
+			var w = 70, s = 10, is = 2 * s
 			var x = (canvas.width - items.length * (w + s) + s) / 2
 			var y = canvas.height - w - s
 
-			ctx.lineWidth = 0.7 * s
 			for ( var i in items) {
-				ctx.strokeStyle = items[i].selected ? '#ff0000' : '#0000ff'
-				ctx.strokeRect(x, y, w, w)
+				ctx.fillStyle = items[i].selected ? '#ff0000' : '#0000ff'
+				ctx.fillRect(x, y, w, w)
+				ctx.drawImage(items[i].img, x + is/2, y + is/2, w - is, w - is);
 				x += w + s
 			}
 		}
