f450c74e25157f67f0f231dca59a78b2dc571e60
Anatolii Yakushko <shaddyx@gmail.com>
2017-10-08 11:29:47 +0300

    [+] added getObjectsAt function

--- js/lib.js
+++ js/lib.js
@@ -3,23 +3,31 @@ _patch$gamelib = {
     _$patchAt: 'lib/',
 
     getSolid: function(x, y) {
-        x = Math.floor(x)
-        y = Math.floor(y)
+        x = Math.floor(x);
+        y = Math.floor(y);
 
-        let solid
+        let solid;
         this._.lab.camera._ls.forEach(function(e) {
             if (e.alive && e.solid
                     && Math.floor(e.x) === x
                     && Math.floor(e.y) === y) {
-                solid = e
+                solid = e;
             }
-        })
-        return solid
+        });
+        return solid;
     },
 
     isFree: function(x, y) {
-        let solid = this.getSolid(x, y)
+        let solid = this.getSolid(x, y);
         return !solid
     },
 
-}
+    getObjectsAt: function(x, y){
+        x = Math.floor(x);
+        y = Math.floor(y);
+
+        this._.lab.camera._ls.filter(e => e.alive && Math.floor(e.x) === x && Math.floor(e.y) === y );
+        return solid;
+    }
+
+};
