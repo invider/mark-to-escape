243722f5c3e493e6053bc7c7a581061bacb851dc
Anatolii Yakushko <shaddyx@gmail.com>
2017-10-08 23:33:37 +0300

    [+] added lvl 6

--- js/levels/level6.js
+++ js/levels/level6.js
@@ -1,19 +1,23 @@
  this["@lib/levels/level6"] =
     "env/player/markers/right=2\n"+
-    "env/player/markers/down=1\n"+
-    "env/player/markers/up=1\n"+
-    "env/player/markers/speedUp" +
+    "env/player/markers/down=3\n"+
+    "env/player/markers/up=0\n"+
+    "env/player/markers/left=1\n"+
+    "env/player/markers/speedUp=2\n" +
     "env/player/goal=30\n"+
     "env/player/levelName=Psycho\n"+
     "^^^SETTINGS^^^"+
     "^^^MACROS^^^" +
-    "*****************************************************\n" +
-    "*   >                             W                 *\n" +
-    "*                                 W                 *\n" +
-    "*                                 W                 *\n" +
-    "*   >                         @                 X   *\n" +
-    "*                                 W                 *\n" +
-    "*                                 W                 *\n" +
-    "*   >                             W                 *\n" +
-    "*                                 W                 *\n" +
-    "*****************************************************\n";
+     "*****************************************************\n" +
+     "*                          w             @     <    *\n" +
+     "*                wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww*\n" +
+     "*                              wwwwwwwwwwwwwwwwwwwww*\n" +
+     "*                              ww                   *\n" +
+     "*                              w                    *\n" +
+     "*                              ww                   *\n" +
+     "*               WWWWWWWWWWWWWWWWWWWWWWWWWWW         *\n" +
+     "*               W                                   *\n" +
+     "*               W                                   *\n" +
+     "*               W wwwwwwwwwwwwwwwwwwwwwwwwww wwwwwww*\n" +
+     "*               W       X                   W       *\n" +
+     "*****************************************************\n";
